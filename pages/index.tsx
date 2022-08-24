003d0618ab34a76fe7aa450d9e74d519d42305a8
Richard Gwati <haggio0844@gmail.com>
2022-08-24 21:08:19 +0200

    Loading new site 👾

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,13 @@
-import type { NextPage } from 'next'
+import type { NextPage } from 'next';
 
-import Nav from '../components/Nav';
-import Leftbar from '../components/Leftbar'
-import Rightbar from '../components/Rightbar'
-import Footer from '../components/Footer'
-import Box from '@mui/material/Box';
-
-import Stack  from '@mui/material/Stack'
+import Loading from '../components/Design/Loading';
 
 const Home: NextPage = () => {
+	return (
+		<>
+			<Loading/>
+		</>
+	);
+};
 
-  return (
-    <Box>
-      <Nav />
-      <Stack direction={{xs: 'column', sm: 'row' }} spacing={{ xs: 1, sm: 2, md: 4 }} justifyContent='space-between'>
-        <Leftbar />
-        <Rightbar />
-     </Stack>
-        <Footer />
-    </Box>
-  )
-}
-
-export default Home
+export default Home;
