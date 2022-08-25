926e479c9f36e021a9c031ae0f73ed2a116382aa
Richard Gwati <haggio0844@gmail.com>
2022-08-25 19:29:53 +0200

    Landing page ✈️

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,19 @@
 import type { NextPage } from 'next';
+import { useState } from 'react';
 
 import Loading from '../components/Design/Loading';
+import Nav from '../components/Nav';
 
 const Home: NextPage = () => {
-	return (
-		<>
-			<Loading/>
-		</>
-	);
+	const [loading, setLoading] = useState(true);
+
+	setTimeout(() => {
+		setLoading(false);
+	}, 3000);
+
+	return (<>{loading ? <Loading /> : <>
+		<Nav />
+	</>}</>);
 };
 
 export default Home;
