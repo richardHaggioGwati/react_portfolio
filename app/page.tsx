036d0cba953cf70a6b998028595bc131f1271e7c
Richard Gwati <haggiog0844@gmail.com>
2023-03-14 20:19:08 +0200

    feat: new layout and components

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,5 @@
-import Hero from '../components/Hero';
-import Header from '../components/Header';
-
 const Home = () => {
-  return (
-    <div className="bg-[#1c1e2b] text-gray-200 h-screen snap-y snap-mandatory overflow-scroll z-0">
-      <Header />
-
-      <section id="hero" className="snap-start">
-        <Hero />
-      </section>
-    </div>
-  );
+  return <div>Hello New world</div>;
 };
 
 export default Home;
